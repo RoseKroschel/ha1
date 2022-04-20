e5b7f5a44d5b0d096a189a58a246eacd314de6d7
rosekroschel <Rose.Kroschel@Student.HTW-Berlin.de>
2022-04-21 01:07:02 +0200

    2. Test: Rot -> Fixed

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -34,6 +34,10 @@ public class Calculator {
         if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
 
         screen = screen + digit;
+
+        if(screen.length() > 9){
+            screen = screen.substring(0,9);
+        }
     }
 
     /**
